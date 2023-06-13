33f132bdf50913259b9231b593995fb32bd39694
Ramzes Juldiyev <dzhuldievramzes@gmail.com>
2023-06-13 11:32:03 +0500

    Hello world!

--- some_file.py
+++ some_file.py
@@ -1,1 +1,8 @@
 print('Hello, Ramzes!')
+
+print('I have made one changing, so i shall commit it')
+print('There is a new local changes')
+print('Hello')
+
+for i in range(7):
+    print('Hello, world!')
